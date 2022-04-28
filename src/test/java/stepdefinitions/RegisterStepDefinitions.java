d69070c2785625c39e07cb4db03d2018b861b1aa
luhooor <ylk.osd@gmail.com>
2022-04-29 05:44:09 +0700

    added step definitions for registration test case

---
+++ src/test/java/stepdefinitions/RegisterStepDefinitions.java
@@ -0,0 +1,34 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import services.bringeeeAPI;
+
+public class RegisterStepDefinitions {
+    bringeeeAPI bringeee = new bringeeeAPI();
+
+    @When("user send POST request to register as {string}")
+    public void userSendPOSTRequestToRegisterAs(String role) throws Exception {
+        if (role.equalsIgnoreCase("customer")) {
+            bringeee.registerCustomer("normal");
+        } else {
+            bringeee.registerDriver("normal");
+        }
+    }
+
+    @And("return body is matched with {string}")
+    public void returnBodyIsMatchedWith(String schema) {
+    }
+
+    @When("user send POST request to register as {string} with case: {string}")
+    public void userSendPOSTRequestToRegisterAsWithCase(String role, String condition) throws Exception {
+
+        if (role.equalsIgnoreCase("customer")) {
+            bringeee.registerCustomer(condition);
+        } else {
+            bringeee.registerDriver(condition);
+        }
+
+
+    }
+}
