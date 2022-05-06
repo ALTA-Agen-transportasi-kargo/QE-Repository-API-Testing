d65e1d22870c3854ad6b02ff6d45b71e892ee29f
Meilisa Putri <meilisaputridwicahya@gmail.com>
2022-05-06 20:17:42 +0700

    added stepdefinitions for delete customer for admin

--- src/test/java/stepdefinitions/DeleteCustomerStepDefinitions.java
+++ src/test/java/stepdefinitions/DeleteCustomerStepDefinitions.java
@@ -1,2 +1,26 @@
-package stepdefinitions;public class DeleteCustomerStepDefinitions {
+package stepdefinitions;
+
+import io.cucumber.java.en.When;
+import services.bringeeeAPI;
+
+public class DeleteCustomerStepDefinitions {
+    bringeeeAPI bringeee = new bringeeeAPI();
+
+    @When("admin send DELETE request to delete customer account with id {int}")
+    public void adminSendDELETERequestToDeleteCustomerAccountWithId(int id) throws Exception {
+        bringeee.setToken("admin");
+        bringeee.deleteCustomerID(id, bringeee.getToken());
+    }
+
+    @When("admin send DELETE request to delete customer account with id {string}")
+    public void adminSendDELETERequestToDeleteCustomerAccountWithIdId(String id) throws Exception {
+        bringeee.setToken("admin");
+        bringeee.deleteCustomerID(id, bringeee.getToken());
+    }
+
+    @When("admin send DELETE request to delete customer account but not login")
+    public void adminSendDELETERequestToDeleteCustomerAccountButNotLogin() throws Exception {
+        bringeee.setToken("noLogin");
+        bringeee.deleteCustomerID(1, bringeee.getToken());
+    }
 }
