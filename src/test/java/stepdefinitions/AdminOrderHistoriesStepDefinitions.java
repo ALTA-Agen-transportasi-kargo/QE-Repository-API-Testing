257cbc83e6a0c60dcbec173bc1b4c326a0acac09
Meilisa Putri <meilisaputridwicahya@gmail.com>
2022-05-11 16:30:01 +0700

    added stepdefinitions for order histories by admin

--- src/test/java/stepdefinitions/AdminOrderHistoriesStepDefinitions.java
+++ src/test/java/stepdefinitions/AdminOrderHistoriesStepDefinitions.java
@@ -1,2 +1,32 @@
-package stepdefinitions;public class AdminOrderHistoriesStepDefinitions {
+package stepdefinitions;
+
+import io.cucumber.java.en.When;
+import services.bringeeeAPI;
+
+public class AdminOrderHistoriesStepDefinitions {
+    bringeeeAPI bringeee = new bringeeeAPI();
+
+    @When("admin send GET request order timeline with id: {int}")
+    public void adminSendGETRequestOrderTimelineWithId(int id) throws Exception {
+        bringeee.setToken("admin");
+        bringeee.getOrderHistoriesAdmin(id, bringeee.getToken());
+    }
+
+    @When("admin send GET request order timeline with invalid id: {int}")
+    public void adminSendGETRequestOrderTimelineWithInvalidId(int id) throws Exception {
+        bringeee.setToken("admin");
+        bringeee.getOrderHistoriesAdmin(id, bringeee.getToken());
+    }
+
+    @When("admin send GET request order timeline with invalid id: {string}")
+    public void adminSendGETRequestOrderTimelineWithInvalidId(String id) throws Exception {
+        bringeee.setToken("admin");
+        bringeee.getOrderHistoriesAdmin(id, bringeee.getToken());
+    }
+
+    @When("admin send GET request order timeline")
+    public void adminSendGETRequestOrderTimeline() throws Exception {
+        bringeee.setToken("noLogin");
+        bringeee.getOrderHistoriesAdmin(4, bringeee.getToken());
+    }
 }
